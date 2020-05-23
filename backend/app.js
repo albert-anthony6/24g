9d1cf8227724288fe2bf092628a2a055d3c7e6ef
Albert Anthony <albert.anthony1999@gmail.com>
2020-05-23 00:08:19 -0500

    Defined cookie and set security packages for backend

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -8,11 +12,30 @@ const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// MIDDLEWARES
+// GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Development logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
-    app.use(express.json()); // BODY PARSER
 }
+
+// Limit requests from same IP
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+app.use(express.json({ limit: '10kb' })); // BODY PARSER
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS attacks
+app.use(xss());
 
 app.use('/api/v1/videos', videoRouter);
 app.use('/api/v1/users', userRouter);
